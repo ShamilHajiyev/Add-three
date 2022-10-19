9c86cbdfa8bac8507b15cb8dae8918416b21d50f
ShamilHajiyev <shamilnh@code.edu.az>
2022-10-19 18:18:01 +0400

    initial commit

---
+++ Overloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nConverted number:\n" + Convert3(input));
+        }
+        public static int Convert3(int input)
+        {
+            int number = input;
+            int counter = 0;
+            while (number != 0)
+            {
+                number /= 10;
+                counter++;
+            }
+
+            int coeff = 1;
+            for (int i = 0; i < counter; i++)
+            {
+                coeff *= 10;
+            }
+            int newNumber = input + 3 * coeff;
+            return newNumber;
+        }
+    }
+}
